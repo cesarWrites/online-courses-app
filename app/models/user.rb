774eec011cf45b071c7bc81a16b57f622392d6c5
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-09 21:50:56 +0300

    update validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
     has_secure_password
+    validates :username, presence: true, length: {minimum: 8}
+    validates :password, uniqueness: true
 end
