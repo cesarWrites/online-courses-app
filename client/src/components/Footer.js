fc00ed36b1f28f266d1f68f03f4eaf0485d88a51
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-10 17:13:08 +0300

    craete teh fotetr cmponent

---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './styles/Footer.css';
+
+function Footer(){
+    return(
+        <div className="foot-div">
+<footer>
+    <nav className="navbar-1">
+    <ul>
+        <li><a href="">About us</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href="">Partners</a></li>
+        <li><a href="">become an intstructor</a></li>
+    </ul>
+    </nav>
+  <p>designed by: Sarah Mukuti <br/>
+  <a href="sarahm@gmail.com">sarahmukuti@gmail.com</a></p>
+  <hr/>
+  <p><span>&#169;</span>2022<p>Prodev Learning</p></p>
+
+</footer>
+        </div>
+    )
+
+}
+export default Footer;
